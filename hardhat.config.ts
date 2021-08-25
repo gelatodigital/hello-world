38dbdc4009f82a73a825303483aeab60f299fa7b
gitpusha <gitpusha@gmail.com>
2021-08-25 14:35:03 +0200

    config: init

---
+++ hardhat.config.ts
@@ -0,0 +1,56 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+// PLUGINS
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+
+// Process Env Variables
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
+
+const PK = process.env.PK;
+const ALCHEMY_ID = process.env.ALCHEMY_ID;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+
+  // hardhat-deploy
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+
+  networks: {
+    ropsten: {
+      accounts: PK ? [PK] : [],
+      chainId: 3,
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_ID}`,
+    },
+  },
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY ? ETHERSCAN_API_KEY : "",
+  },
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+        },
+      },
+    ],
+  },
+
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+};
+
+export default config;
