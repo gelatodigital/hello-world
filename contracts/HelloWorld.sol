f2fd8bacc005faa4dcfac45ae41a59d2fc5eb547
gitpusha <gitpusha@gmail.com>
2021-08-26 19:22:18 +0200

    fix: test

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.7;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Gelatofied} from "./gelato/Gelatofied.sol";
-import {console} from "hardhat/console.sol";
 
 contract HelloWorld is Ownable, Gelatofied {
     uint256 public interval;
@@ -11,9 +10,8 @@ contract HelloWorld is Ownable, Gelatofied {
 
     event LogHelloWorld();
 
-    constructor(address payable _gelato) Gelatofied(_gelato) {
-        lastCallTime = block.timestamp; // solhint-disable-line
-    }
+    // solhint-disable-next-line no-empty-blocks
+    constructor(address payable _gelato) Gelatofied(_gelato) {}
 
     function setInterval(uint256 _interval) external onlyOwner {
         interval = _interval;
@@ -21,16 +19,15 @@ contract HelloWorld is Ownable, Gelatofied {
 
     function helloWorld(uint256 _paymentAmount)
         external
-        payable
         gelatofy(_paymentAmount, Gelatofied.ETH)
     {
         require(canExec(), "helloWorld: not canExec");
-        console.log("%d", _paymentAmount);
         lastCallTime = block.timestamp; // solhint-disable-line
         emit LogHelloWorld();
     }
 
     function canExec() public view returns (bool) {
-        return (lastCallTime + interval) <= block.timestamp; // solhint-disable-line
+        if (lastCallTime == 0) return true;
+        return lastCallTime + interval <= block.timestamp; // solhint-disable-line
     }
 }
