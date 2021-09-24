dacb34c94bb607bbe32d8a33b933326222097a5d
Daniel Cabrera <daniel@gelato.digital>
2021-09-24 18:13:34 +0200

    chore: matic and fantom deployment (#10)

--- deploy/HelloWorld.deploy.ts
+++ deploy/HelloWorld.deploy.ts
@@ -6,7 +6,11 @@ import { sleep } from "../src/utils";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  if (hre.network.name === "ropsten") {
+  if (
+    hre.network.name === "ropsten" ||
+    hre.network.name === "matic" ||
+    hre.network.name === "fantom"
+  ) {
     console.log(
       `Deploying HelloWorld to ${hre.network.name}. Hit ctrl + c to abort`
     );
@@ -37,7 +41,11 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 export default func;
 
 func.skip = async (hre: HardhatRuntimeEnvironment) => {
-  const shouldSkip = hre.network.name === "ropsten";
+  const shouldSkip =
+    hre.network.name === "ropsten" ||
+    hre.network.name === "matic" ||
+    hre.network.name === "fantom";
+
   return shouldSkip ? true : false;
 };
 
